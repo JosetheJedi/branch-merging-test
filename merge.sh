3f8fc3a2f3a75184c55e1549f4d527a31de05bba
Jose Hernandez-Uribe <josefhu15@gmail.com>
2020-07-07 19:40:14 -0700

    updated merge.sh

--- merge.sh
+++ merge.sh
@@ -3,6 +3,16 @@
 function merging_stage()
 {
     echo "merging_stage"
+    git branch -a
+
+    if ! currentBranch=$(git symbolic-ref -q HEAD)
+    then
+        echo "We are not currently on a branch!"
+        echo "Attempting to checkout integration branch!"
+        git checkout -b integration origin/integration
+    else
+        echo "We are currently on branch: ${currentBranch}"
+    fi
 }
 
 function building_stage()
@@ -22,8 +32,22 @@ function deployment_stage()
 
 #check what stage we are on
 
-stages=("merging_stage", "building_stage", "testing_stage", "deployment_stage")
+stages=("merging_stage" "building_stage" "testing_stage" "deployment_stage")
 
+if [ $1 = ${stages[0]} ]
+then
+    merging_stage
+elif [ $1 = ${stages[1]} ]
+then
+    building_stage
+elif [ $1 = ${stages[2]} ]
+then
+    testing_stage
+elif [ $1 = ${stages[3]} ]
+then
+    deployment_stage
+else
+    echo "That stage: $1 is not defined"
+    exit 1
+fi
 
-
-echo ${stages[0]}
